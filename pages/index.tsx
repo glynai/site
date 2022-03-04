e1ace5142c30625b7bd60b69a563098cbc5fa921
Jacob Marshall <jacob@omg.lol>
2022-03-04 16:48:28 +0000

    :lipstick: Polish homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,31 @@
-import { Box, Center, Container, Heading, Text, VStack } from '@chakra-ui/react'
-
+import { Box, Button, Center, Container, Flex, Heading, Text, useColorModeValue, useToast, VStack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import vercelBadge from "../public/powered-by-vercel.svg"
 
 const Home: NextPage = () => {
+  const toast = useToast()
+  const primary = useColorModeValue("blue.500", "blue.200")
+
   return (
     <>
-    <Container>
-      <Center h="100vh">
-        <VStack>
-          <Heading size="4xl">Glyn Hack</Heading>
-          <Text fontSize="xl">Learn to code with Glyn Students</Text>
-        </VStack>
-      </Center>
-    </Container>
+      <Container>
+        <Center h="100vh">
+          <VStack>
+            <Text fontSize="lg" fontWeight="bold" color={primary}>Glynhack</Text>
+            <Heading size="2xl">Code in a community</Heading>
+            <Text textAlign="center" pt={4} pb={6}>Don't fight those errors alone. Bang your face on a keyboard in public, because that will fix all your problems.</Text>
+            <a href="http://glynhack.vercel.app/">
+              <Button colorScheme='blue' size="lg">Join the waitlist</Button>
+            </a>
+          </VStack>
+          <Box pos="absolute" bottom={5}>
+            <a target="_blank" href="http://vercel.com/?utm_source=glynhack&utm_campaign=oss">
+              <Image src={vercelBadge} />
+            </a>
+          </Box>
+        </Center>
+      </Container>
     </>
   )
 }
