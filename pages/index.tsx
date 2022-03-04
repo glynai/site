4429ac415049b185e2e76e39d23b24b51a414544
Jacob Marshall <jacob@omg.lol>
2022-03-04 17:01:19 +0000

    🔒️ Use https

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,12 +15,12 @@ const Home: NextPage = () => {
             <Text fontSize="lg" fontWeight="bold" color={primary}>Glynhack</Text>
             <Heading size="2xl">Code in a community</Heading>
             <Text textAlign="center" pt={4} pb={6}>Don&apos;t fight those errors alone. Bang your face on a keyboard in public, because that will fix all your problems.</Text>
-            <a href="http://glynhack.vercel.app/">
+            <a href="https://glynhack.vercel.app/">
               <Button colorScheme='blue' size="lg">Join the waitlist</Button>
             </a>
           </VStack>
           <Box pos="absolute" bottom={5}>
-            <a target="_blank" rel="noreferrer" href="http://vercel.com/?utm_source=glynhack&utm_campaign=oss">
+            <a target="_blank" rel="noreferrer" href="https://vercel.com/?utm_source=glynhack&utm_campaign=oss">
               <Image src={vercelBadge} />
             </a>
           </Box>
